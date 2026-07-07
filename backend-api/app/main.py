22bb0a8fdd3d2a6dd25a1143034a33e24eab4411
Fernando Gianfranco Enriquez Ventura <2013200959@untels.edu.pe>
2026-07-07 18:14:01 -0500

    Integra frontend con backend y habilita predicción DASS-21

--- backend-api/app/main.py
+++ backend-api/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes import router
+
 
 app = FastAPI(
 
@@ -11,5 +13,32 @@ app = FastAPI(
     description="API para la detección temprana de depresión, ansiedad y estrés mediante Machine Learning."
 
 )
+
+# =====================================================
+# CONFIGURACIÓN CORS
+# =====================================================
+
+app.add_middleware(
+
+    CORSMiddleware,
+
+    allow_origins=[
+
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+
+    ],
+
+    allow_credentials=True,
+
+    allow_methods=["*"],
+
+    allow_headers=["*"],
+
+)
+
+# =====================================================
+# RUTAS
+# =====================================================
 
 app.include_router(router)
